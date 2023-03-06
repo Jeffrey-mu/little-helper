2d44b66730d6a2898adc7356a11d24fae3f9e264
Jeffrey <1115378579@qq.com>
2023-03-06 23:48:07 +0800

    wip: 持续开发中

---
+++ miniprogram/pages/more/select/index.js
@@ -0,0 +1,159 @@
+// pages/luckDraw/index.js
+const app = getApp()
+Page({
+  data: {
+    blocks: [{
+      padding: '13px',
+      background: '#E8D5C4'
+    }],
+    prizes: [{
+        fonts: [{
+          text: 'YES',
+          fontColor: '#ffffff',
+          top: '50%'
+        }],
+        range: 10,
+        background: '#e9e8fe'
+      },
+      {
+        fonts: [{
+          text: 'NO',
+          fontColor: '#ffffff',
+          top: '50%'
+        }],
+        range: 10,
+        background: '#b8c5f2'
+      },
+    ],
+    buttons: [
+      {
+        radius: '41px',
+        background: '#E8D5C4'
+      },
+      {
+        radius: '40px',
+        background: '#afc8ff'
+      },
+      {
+        radius: '35px',
+        background: '#7286D3',
+        pointer: true,
+        fonts: [{
+          text: '开始',
+          top: '-20px',
+          fontColor: '#ffffff'
+        }]
+      },
+    ],
+    items: [],
+    show: true,
+    inputValue: '',
+    colors: ["#6E85B7", "#E7AB9A", "#95BDFF"],
+    value: ""
+  },
+  onLoad() {
+    if (wx.getStorageSync('eatlist')) {
+      this.setData({
+        items: wx.getStorageSync('eatlist')
+      })
+      setTimeout(() => this.generate())
+    }
+  },
+  onUnload: function () {
+    wx.setStorageSync('eatlist', this.data.items)
+  },
+  randomColor() {
+    var x = Array.from({
+      length: 30
+    }, (v, i) => i / 29);
+
+    // 生成一个蓝色渐变数组
+    var colors = x.map(function (value) {
+      var r = Math.round(value * 255);
+      var g = Math.round(value * 0);
+      var b = Math.round(value * 255);
+      return 'rgba(' + b + ',' + g + ',' + r + ', 1)';
+    });
+
+    var hexColors = colors.map((value) => {
+      var rgb = value.substring(5, value.length - 2).split(',');
+      var r = parseInt(rgb[0]);
+      var g = parseInt(rgb[1]);
+      var b = parseInt(rgb[2]);
+      return '#' + this.toHex(r) + this.toHex(g) + this.toHex(b);
+    });
+    return hexColors
+  },
+  toHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+  },
+  inputChange: function (e) {
+    this.setData({
+      inputValue: e.detail.value
+    })
+  },
+  generate() {
+    this.setData({
+      prizes: (this.data.items ? this.data.items : wx.getStorageSync('eatlist')).map((item, index) => {
+        return {
+          fonts: [{
+            text: item.label,
+            fontColor: '#ffffff',
+            top: '50%'
+          }],
+          range: 10,
+          background: this.data.colors[index],
+        }
+      })
+    });
+
+  },
+  addItem: function () {
+    var items = this.data.items;
+    var inputValue = this.data.inputValue;
+    if (inputValue) {
+      items.push({
+        label: inputValue
+      });
+      this.setData({
+        items: items,
+        inputValue: ''
+      });
+    }
+  },
+  deleteItem: function (e) {
+    var index = e.currentTarget.dataset.index;
+    var items = this.data.items;
+    items.splice(index, 1);
+    this.setData({
+      items: items
+    });
+  },
+  showAdd() {
+    console.log(this.randomColor())
+    this.setData({
+      show: !this.data.show
+    })
+    this.generate()
+  },
+  start() {
+    // 获取抽奖组件实例
+    const child = this.selectComponent('#myLucky')
+    // 调用play方法开始旋转
+    child.lucky.play()
+    // 用定时器模拟请求接口
+    setTimeout(() => {
+      // 3s 后得到中奖索引 (假设抽到第0个奖品)
+      // const index = 0
+      // 调用stop方法然后缓慢停止
+      child.lucky.stop()
+    }, 3000)
+  },
+  end(event) {
+    // 中奖奖品详情
+    this.setData({
+      value: event.detail.fonts[0].text
+    })
+  }
+})
---
+++ miniprogram/pages/more/workPlan/index.js
@@ -0,0 +1,114 @@
+// miniprogram/pages/more/workPlan/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        title: `${new Date().getMonth() + 1 + '.' + new Date().getDate()} 工作计划`,
+        workList: [
+            '亚马逊账号日常维护',
+            'dv优化，上素材和订单项',
+            'Start.IO数据api对接',
+            '产品化开发，GoogleAPI对接',
+            '新客审核质控',
+            '整体流量质控，GA监测反馈',
+            '客户服务平台客户管理细节设计',
+        ]
+    },
+    copyEvent() {
+        let workPlan = [this.data.title, ...this.data.workList]
+        wx.setClipboardData({
+            data: workPlan.map((item, index) => (index ? index + '、' + item : item)).filter(item => item)
+            .join('\n'),
+            success: function () {
+                wx.showToast({
+                    title: '复制成功',
+                    icon: 'none'
+                })
+            }
+        })
+
+    },
+    addEvent() {
+        this.setData({
+            workList: [...this.data.workList, '']
+        })
+    },
+    inputChange(e) {
+        const index = e.currentTarget.dataset.index;
+        const value = e.detail.value;
+        const items = this.data.workList;
+        items[index] = value;
+        this.setData({
+            workList: items
+        });
+    },
+    delEvent(e) {
+        const index = e.currentTarget.dataset.index;
+        const items = this.data.workList;
+        items.splice(index, 1)
+        this.setData({
+            workList: items
+        });
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        if (wx.getStorageSync('workPlan')) {
+            this.setData({
+                workList: wx.getStorageSync('workPlan')
+            })
+        }
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+        
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+        wx.setStorageSync('workPlan', this.data.workList)
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
